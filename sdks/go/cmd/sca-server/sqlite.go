310a682e90babca95e4dcd523dcb11b67aacf04a
perfect <mahdi_1373@yahoo.com>
2026-05-09 13:05:38 +0200

    Add `sca-server-pg` command with Postgres-backed storage support and developer tools for `InstantApproval`.

--- sdks/go/cmd/sca-server/sqlite.go
+++ sdks/go/cmd/sca-server/sqlite.go
@@ -5,8 +5,8 @@ package main
 import (
 	"database/sql"
 
-	"github.com/shadownet-protocol/shadownet-go/internal/storesqlite"
 	"github.com/shadownet-protocol/shadownet-go/pkg/sca"
+	"github.com/shadownet-protocol/shadownet-go/pkg/storesqlite"
 )
 
 func openSQLiteStores(dsn string) (sca.SessionStore, sca.IssuanceStore, sca.RevocationStore, *sql.DB, error) {
